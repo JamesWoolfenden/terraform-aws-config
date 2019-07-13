8de54954276ad6ccede50e887e3f2c84a0e6d6d9
James Woolfenden <James.Woolfenden@slalom.com>
2019-07-13 22:12:31 +0100

    migrate to v0.12

--- example/exampleA/module.config.tf
+++ example/exampleA/module.config.tf
@@ -2,4 +2,5 @@ module "config" {
   source     = "../../"
   name       = "aws-config"
   log_bucket = "config-logs"
+  common_tags= var.common_tags
 }
---
+++ example/exampleA/variables.tf
@@ -0,0 +1,6 @@
+variable "common_tags" {
+  type=map
+  default={
+      Name="Security-config"
+  }
+}

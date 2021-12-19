c821aa19dbfe435ec8be32da5cceed2930eec231
James Woolfenden <James.Woolfenden@gmail.com>
2021-12-19 11:28:00 +0000

    refresh

--- aws_sns_topic.config.tf
+++ aws_sns_topic.config.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic" "config" {
   name              = local.config_name
-  kms_master_key_id = "alias/aws/sns"
+  kms_master_key_id = aws_kms_key.s3.arn
   tags              = var.common_tags
 }

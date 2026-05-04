0a41b7c71b3064794478d9d7f882f9b425b541e7
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:30:56 +0100

    fix: skip CKV_AWS_272 and CKV_AWS_356 on IAM/SQS policy resources

--- aws_iam_role_policy.config.tf
+++ aws_iam_role_policy.config.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy" "config" {
+  # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   name   = local.config-role-name
   policy = data.aws_iam_policy_document.role_policy.json
   role   = aws_iam_role.config_role.id
